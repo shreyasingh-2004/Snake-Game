88088afeb36a21b44da29ad7dfdf27af7b27730d
shreyasingh-2004 <156939544+shreyasingh-2004@users.noreply.github.com>
2025-05-16 13:10:00 +0530

    Delete fruit1.js

--- fruit1.js
+++
@@ -1,8 +0,0 @@
-export function wait(ms = 0) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-  
-export function randomElementFromArray(arr) {
-    const element = arr[Math.floor(Math.random() * arr.length)];
-    return element;
-  }
